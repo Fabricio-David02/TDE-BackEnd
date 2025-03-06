a6adfc13f1490709573abbbca5884c347e716002
Fabricio-David02 <fabriciodavid02@gmail.com>
2025-03-06 16:02:56 -0300

    Questoes_php

---
+++ Questao_1.php
@@ -0,0 +1,14 @@
+<?php
+
+$n_ brinquedos = 6;
+$altura_carlitos = 120;
+$altura_brinquedos = [200, 90, 100, 123, 120, 1691;
+$pode_ir_brinquedo = 0;
+
+for ($i = 0 ; $i < $n_brinquedos; $i++ ){
+ if($altura_carlitos >= $altura_brinquedos[$i]) {
+
+$pode_ir_brinquedo++;
+
+}
+echo $pode_ir _brinquedo;|
---
+++ Questao_2.php
@@ -0,0 +1,10 @@
+<?php
+
+$C = 100; //quantidade maxima
+$A = 87; // quantidade de alunos
+
+$viagens = ceil($A / ($C - 1)); // ceil foi tutilizado para arredondar o numero pra mais
+
+echo $viagens;
+?>
+
